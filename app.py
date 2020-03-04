321a9aa67eb85be81b1b4786e371b2a8cbca1b03
Dalton Saffe <daltonsaffe@gmail.com>
2020-03-03 23:51:04 -0500

    sync sqlite w flask sqlalchemy

--- app.py
+++ app.py
@@ -2,18 +2,23 @@ import os
 from flask import Flask, request, redirect, render_template
 from s3 import upload_file, list_files
 from flask_sqlalchemy import SQLAlchemy
-
+import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////db/art.sqlite'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/daltonsaffe/devel/art-camp-combinations/db/art.sqlite'
+
 db = SQLAlchemy(app)
 
 class Art(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(120), unique=True, nullable=False)
+    url = db.Column(db.String(120), unique=True, nullable=False)
     title = db.Column(db.String(120), unique=True, nullable=False)
     artist = db.Column(db.String(120), unique=False, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now)
 
 
 UPLOAD_DIR = "uploads"
@@ -23,6 +28,14 @@ BUCKET = 's3-bucket-name-here'
 @app.route('/')
 def hello_world():
     return '<h1>Hello, World!</h1>'
+
+@app.route('/<url>/<title>/<artist>')
+def index(url, title, artist):
+    art = Art(url=url, title=title, artist=artist)
+    db.session.add(art)
+    db.session.commit()
+
+    return '<h1>Added new art piece</h1>'
 
 
 @app.route('/upload', methods=['POST'])
